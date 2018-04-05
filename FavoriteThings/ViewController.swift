45493e6450712a8a63beafba903d69f5541448b3
Praneet Chakraborty <p.chak@outlook.com>
2018-04-05 12:15:11 -0400

    Initial Commit

---
+++ FavoriteThings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FavoriteThings
+//
+//  Created by Praneet Chakraborty on 4/5/18.
+//  Copyright © 2018 Praneet Chakraborty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
